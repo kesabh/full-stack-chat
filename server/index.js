4bfef7774165a9be6d0ecf867264e27dd4ed62d9
Keshav Goel <goelkeshav1999@gmail.com>
2023-12-28 00:00:23 +0530

    configure dotenv for express server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
 import { connectMongoDB } from "./config/monogdb.js";
 import { authController } from "./controllers/authController.js";
 
 const app = express();
 
+dotenv.config();
 connectMongoDB();
 
 app.use(express.json());
